9e373b188519f8b2dd8182783c4ef374402ab4f1
ml2439 <ml2439@cornell.edu>
2017-07-04 10:55:30 -0400

    Stats now get access to state

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  TextInput,
+  View,
+  DatePickerIOS,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Foundation';
+import { MKColor, MKButton } from 'react-native-material-kit';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class Stats extends Component {
+const SubmitButton = MKButton.coloredButton()
+  .withText('SUBMIT')
+  .build()
+
+class Stats extends Component {
+
   static navigationOptions = {
     tabBarLabel: 'Stats',
     tabBarIcon: ({ tintColor }) => (
@@ -17,10 +27,21 @@ export default class Stats extends Component {
       />
     )
   }
+
+  componentWillMount() {
+    this.props.loadGoal();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Stats page</Text>
+        <Text>Stats</Text>
+
+        <View>
+          <Text>Deadline: {this.props.ddl.toString()}</Text>
+          <Text>To read: {this.props.num}</Text>
+          <Text>Books finished: {this.props.books.filter(b => b.finished).length}</Text>
+        </View>
       </View>
     );
   }
@@ -29,8 +50,15 @@ export default class Stats extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'column',
+    paddingTop: 20,
   },
 });
+
+const mapStateToProps = state => {
+  const { num, ddl } = state.goalReducer;
+  const books = state.bookReducer;
+  return { num, ddl, books }
+}
+
+export default connect(mapStateToProps, actions)(Stats)
