c249da0309149765d0915d4eb56e1a52f4744a95
ml2439 <ml2439@cornell.edu>
2017-07-05 13:58:28 -0400

    Swipe to delete

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -8,54 +8,21 @@ import * as actions from '../actions';
 
 const theme = getTheme();
 
-const styles = StyleSheet.create({
-    card: {
-        height: 50,
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    removeButton: {
-        color: '#ccc',
-        padding: 10,
-    },
-    bookInfo: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    bookName: {
-        fontStyle: 'italic',
-        fontSize: 16,
-        paddingBottom: 6,
-        color: '#444',
-    },
-    bookAuthor: {
-        color: '#555',
-        paddingTop: 6,
-        paddingRight: 10,
-    },
-})
-
-
 const BookItem = props => {
     return (
         <Swipeout
             right={[
                 {
                     text: 'Finish',
-                    onPress: () => props.toggleBook(props.book.bid),
                     backgroundColor: '#00BAAD',
+                    onPress: () => props.toggleBook(props.book.bid),
                 }
             ]}
-            autoClose={true}
-            style={{backgroundColor: props.book.mark}}
-        >
-            <View>
-                <FoundationIcon
-                    name={'x'}
-                    size={20}
-                    style={styles.removeButton}
-                    onPress={() => Alert.alert(
+            left={[
+                {
+                    text: 'Delete',
+                    backgroundColor: 'red',
+                    onPress: () => Alert.alert(
                         'Remove Book',
                         `Sure you want to remove: ${props.book.name}?`,
                         [
@@ -69,15 +36,48 @@ const BookItem = props => {
                             },
                         ],
                         { cancelable: true }
-                    )}
-                />
-                <View style={styles.bookInfo}>
-                    <Text style={styles.bookName}>{props.book.name}</Text>
-                    <Text style={styles.bookAuthor}>{props.book.author}</Text>
-                </View>
+                    )
+                }
+            ]}
+            autoClose={true}
+            style={{ backgroundColor: props.book.mark }}
+        >
+            <View style={styles.bookInfo}>
+                <FoundationIcon name={'book'} size={20} style={styles.bookIcon}/>
+                <Text style={styles.bookName}>{props.book.name}</Text>
+                <Text style={styles.bookAuthor}>{props.book.author}</Text>
             </View>
         </Swipeout>
     )
 }
+
+const styles = StyleSheet.create({
+    removeButton: {
+        color: '#ccc',
+        padding: 10,
+    },
+    bookInfo: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: 50,
+    },
+    bookIcon: {
+        color: '#ccc',
+        padding: 10,
+    },
+    bookName: {
+        fontStyle: 'italic',
+        fontSize: 15,
+        paddingBottom: 5,
+        color: '#444',
+    },
+    bookAuthor: {
+        color: '#555',
+        paddingTop: 5,
+        paddingRight: 10,
+    },
+})
 
 export default connect(null, actions)(BookItem)
